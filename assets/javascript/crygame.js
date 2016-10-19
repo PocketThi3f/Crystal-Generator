a2e3442a38e38f6f7fb314f8b062339bce05ef30
PocketThi3f <krslai@yahoo.com>
2016-10-19 13:32:39 -0700

    powerUp

--- assets/javascript/crygame.js
+++ assets/javascript/crygame.js
@@ -1,51 +1,104 @@
-$(document).ready(function() { 
+//Variables for Scoring
+var wins = 0;
+var losses = 0;
+var score = 0;
+var winningNumber = Math.floor((Math.random() * 120) + 19);
+console.log(winningNumber);
+
+//Variables and  random values for individual gems
+var melonCrystal = Math.floor((Math.random() * 12) + 1);
+var galaxyOpal = Math.floor((Math.random() * 12) + 1);
+var porkGem = Math.floor((Math.random() * 12) + 1);
+var cabbageGem = Math.floor((Math.random() * 12) + 1);
 
 
-			$('.powerGem').on('click', function() {
-                    
-    			var ranNum = [];
+//Gem values updated to update function
+$(document).ready(function(){
 
-        			for (var i = 0, t = 1; i < t; i++) {
+	$('#watermelon').click(function(){
+			score = score + melonCrystal;
+			results();
+		})
 
-            		ranNum.push(Math.round(Math.random() * 11) + 1);
+	$('#opal').click(function(){
+			score = score + galaxyOpal;
+			results();	
+		})
 
-           			$('#goalNumber').html('<span> ' + ranNum + '</span>');
+	$('#porkBelly').click(function(){
+			score = score + porkGem;
+			results();
+		})
 
-        		}
-
-
-        	$('#numberGen').text(matchedNum); 
-                    
-    			var matchedNum = [];
-
-        			for (var i = 19, t = 120; i < t; i++) {
-
-            		matchedNum.push(Math.round(Math.random() * 120));
-
-            		$('#numberGen').html('<span> ' + matchedNum + '</span>');		
-
-        			}
-
-        		// $('#goalNumber').text(countUp);
-
-				// 	if (countUp == numberGen) {
-
-				// 		alert("Winner, winner! Chicken Dinner.");
-				// 		wins++;
-
-				// 	} 
-
-				// 	else (countUp > numberGen) {
-
-				// 		alert('Whisper: The cake is a LIE. Try again!');
-				// 		losses++;
-
-					// });
-
-				// });
-					console.log(matchedNum);
-
-					console.log(ranNum);
-			});
+	$('#cabbage').click(function(){
+			score = score + cabbageGem;
+			results();
+		})
+		
+		$("#randomNumber").append(winningNumber);
+		$("#score").append(score);
 
 });
+
+
+//Appending to the html score/values
+function updateStats(){
+
+	$('#score').empty();
+	$('#score').append(score);
+	$('#score').replaceAll(score);
+
+	$('#wins').empty();
+	$('#wins').append(wins);
+
+	$('#losses').empty();
+	$('#losses').append(losses);
+
+	
+
+}
+
+//Begin the game one more time
+function restart(){
+
+	score = 0;
+	winningNumber = Math.floor((Math.random() * 120) + 19);
+
+	$("#randomNumber").empty();
+	$("#randomNumber").append(winningNumber);
+
+		melonCrystal = Math.floor((Math.random() * 12) + 1);
+		galaxyOpal = Math.floor((Math.random() * 12) + 1);
+		porkGem = Math.floor((Math.random() * 12) + 1);
+		cabbageGem = Math.floor((Math.random() * 12) + 1);
+
+	updateStats();
+
+}
+
+function results (){
+
+	if (score == winningNumber) {
+
+			wins = wins + 1;
+			alert('Congratulations. You Won! Please play again!');
+
+			restart();
+
+			}			
+
+		else if (score > winningNumber) {
+
+			losses = losses + 1;
+			alert('You lost. Please play again!');
+			restart();
+
+			}
+
+		else {
+
+			updateStats();
+
+			}
+
+}
